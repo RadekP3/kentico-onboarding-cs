f1bc58458caacaa107121e5c8ff94dd31e377c40
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-02 12:50:19 +0200

    KA-342 add cleaned up ASP.NET Web Application template

---
+++ TodoListApp/TodoList.Api.Tests/DummyTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TodoList.Api.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void BasicMathTest()
+        {
+            Assert.AreEqual(5, 2 + 3);
+        }
+    }
+}
