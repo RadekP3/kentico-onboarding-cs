0a1468be2478e9dc6da184519c56969dd3efa203
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-15 15:32:21 +0200

    KA-343 create item comparer for testing purposes

---
+++ TodoListApp/TodoList.Api.Tests/Comparers/ItemComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TodoList.Api.Models;
+
+namespace TodoList.Api.Comparers
+{
+    public class ItemComparer: IEqualityComparer<Item>
+    {
+        public bool Equals(Item x, Item y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Id == y.Id && string.Equals(x.Text, y.Text);
+        }
+
+        public int GetHashCode(Item obj)
+        {
+            unchecked
+            {
+                return (obj.Id.GetHashCode() * 397) ^ (obj.Text != null ? obj.Text.GetHashCode() : 0);
+            }
+        }
+    }
+}
