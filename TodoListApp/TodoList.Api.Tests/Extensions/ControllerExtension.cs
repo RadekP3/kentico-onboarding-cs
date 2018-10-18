424849d87ca3f369bc80dfed4f0ba1d029a589a4
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-18 11:18:14 +0200

    KA-343 refactor tests using extension method

---
+++ TodoListApp/TodoList.Api.Tests/Extensions/ControllerExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TodoList.Api.Controllers;
+
+namespace TodoList.Api.Tests.Extensions
+{
+    public static class ControllerExtension
+    {
+        public static async Task<HttpResponseMessage> ExecuteAction(this ItemsController controller, Func<ItemsController, Task<IHttpActionResult>> action)
+        {
+            var result = await action(controller);
+            return await result.ExecuteAsync(CancellationToken.None);
+        }
+    }
+}
--- TodoListApp/TodoList.Api.Tests/ItemsControllerTests.cs
+++ TodoListApp/TodoList.Api.Tests/ItemsControllerTests.cs
@@ -11,6 +11,7 @@ using NUnit.Framework.Internal;
 using TodoList.Api.Comparers;
 using TodoList.Api.Controllers;
 using TodoList.Api.Models;
+using TodoList.Api.Tests.Extensions;
 
 namespace TodoList.Api.Tests
 {
@@ -19,31 +20,27 @@ namespace TodoList.Api.Tests
     {
         readonly Item[] _defaultItems =         
         {
-            new Item() {Id = Guid.Empty, Text = "First"},
-            new Item() {Id = Guid.Empty, Text = "Second"},
-            new Item() {Id = Guid.Empty, Text = "Third"}
+            new Item {Id = Guid.Empty, Text = "First"},
+            new Item {Id = Guid.Empty, Text = "Second"},
+            new Item {Id = Guid.Empty, Text = "Third"}
         };
         readonly ItemComparer _comparer = new ItemComparer();
-
         readonly Item _mockItem = new Item() { Id = Guid.Empty, Text = "Item" };
+        readonly ItemsController _controller = new ItemsController() { Configuration = new HttpConfiguration(), Request = new HttpRequestMessage()};
 
         [Test]
         public async Task GetAsync_ReturnsDefaultItemsWithOkStatusCode()
         {
-            var controller = new ItemsController() {Configuration = new HttpConfiguration(), Request = new HttpRequestMessage()};
-
-            var actionResult = await controller.GetAsync().Result.ExecuteAsync(CancellationToken.None);
+            var actionResult = await _controller.ExecuteAction(controller => controller.GetAsync());
             actionResult.TryGetContentValue(out Item[] itemsFromMessage);
-            Console.WriteLine("items " + itemsFromMessage.Length);
+
             Assert.That(actionResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
             Assert.That(itemsFromMessage, Is.EqualTo(_defaultItems).Using(_comparer));
         }
         [Test]
         public async Task GetAsync_Id_ReturnsFirstItemWithOkStatusCode()
         {
-            var controller = new ItemsController() { Configuration = new HttpConfiguration(), Request = new HttpRequestMessage() };
-
-            var actionResult = await controller.GetAsync(2).Result.ExecuteAsync(CancellationToken.None);
+            var actionResult = await _controller.ExecuteAction(controller => controller.GetAsync(2));
             actionResult.TryGetContentValue(out Item itemFromMessage);
 
             Assert.That(actionResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
@@ -53,9 +50,7 @@ namespace TodoList.Api.Tests
         [Test]
         public async Task  PostAsync_Item_ReturnsFirstItemWithCreatedStatusCode()
         {
-            var controller = new ItemsController() { Configuration = new HttpConfiguration(), Request = new HttpRequestMessage() };
-
-            var actionResult = await controller.PostAsync(_mockItem).Result.ExecuteAsync(CancellationToken.None);
+            var actionResult = await _controller.ExecuteAction(controller => controller.PostAsync(_mockItem));
             actionResult.TryGetContentValue(out Item itemFromMessage);
 
             Assert.That(actionResult.StatusCode, Is.EqualTo(HttpStatusCode.Created));
@@ -64,9 +59,7 @@ namespace TodoList.Api.Tests
         [Test]
         public async Task PutAsync_Id_Item_ReturnSecondItemWithOkStatusCode()
         {
-            var controller = new ItemsController() { Configuration = new HttpConfiguration(), Request = new HttpRequestMessage() };
-
-            var actionResult = await controller.PutAsync(0, _mockItem).Result.ExecuteAsync(CancellationToken.None);
+            var actionResult = await _controller.ExecuteAction(controller => controller.PutAsync(0, _mockItem));
             actionResult.TryGetContentValue(out Item itemFromMessage);
 
             Assert.That(actionResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
@@ -76,14 +69,11 @@ namespace TodoList.Api.Tests
         [Test]
         public async Task DeleteAsync_Id_ReturnsFirstItemWithOkStatusCode()
         {
-            var controller = new ItemsController() { Configuration = new HttpConfiguration(), Request = new HttpRequestMessage() };
-
-            var actionResult = await controller.DeleteAsync(0).Result.ExecuteAsync(CancellationToken.None);
+            var actionResult = await _controller.ExecuteAction(controller => controller.DeleteAsync(0));
             actionResult.TryGetContentValue(out Item itemFromMessage);
 
             Assert.That(actionResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
             Assert.That(itemFromMessage, Is.EqualTo(_defaultItems[0]).Using(_comparer));
         }
-
     }
 }
