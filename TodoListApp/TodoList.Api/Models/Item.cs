5eb826467e58dece6f0f01a48c0a1e20ff360721
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-18 11:19:37 +0200

    KA-343 override ToString in Item

--- TodoListApp/TodoList.Api/Models/Item.cs
+++ TodoListApp/TodoList.Api/Models/Item.cs
@@ -6,5 +6,9 @@ namespace TodoList.Api.Models
     {
         public Guid Id { get; set; }
         public string Text { get; set; } = "";
+        public override string ToString()
+        {
+            return $"Id: {Id}, Text: {Text}";
+        }
     }
 }
