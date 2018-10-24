6c2490066713e7102305bf9774113903a1a74a91
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-24 15:07:57 +0200

    KA-343 convert ToString to arrow function

--- TodoListApp/TodoList.Api/Models/Item.cs
+++ TodoListApp/TodoList.Api/Models/Item.cs
@@ -6,9 +6,6 @@ namespace TodoList.Api.Models
     {
         public Guid Id { get; set; }
         public string Text { get; set; } = "";
-        public override string ToString()
-        {
-            return $"Id: {Id}, Text: {Text}";
-        }
+        public override string ToString() => $"Id: {Id}, Text: {Text}";
     }
 }
