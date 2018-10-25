bcbaecdc60b0fe06844e573977e4ec0c2eb52cd9
Radek Pavelka <42060621+RadekP3@users.noreply.github.com>
2018-10-25 16:00:31 +0200

    KA-343 reformat code and state accessability explicitly

--- TodoListApp/TodoList.Api.Tests/ItemsControllerTests.cs
+++ TodoListApp/TodoList.Api.Tests/ItemsControllerTests.cs
@@ -13,9 +13,9 @@ namespace TodoList.Api.Tests
     [TestFixture]
     public class ItemsControllerTests
     {
-        ItemsController _controller;
+        private ItemsController _controller;
 
-        readonly Item[] _defaultItems =         
+        private readonly Item[] _defaultItems =
         {
             new Item {Id = new Guid("9cece279-9343-4214-b03f-1062a047727e"), Text = "First"},
             new Item {Id = new Guid("5f24635c-e42f-4c99-8156-a8b94b213d0b"), Text = "Second"},
@@ -26,10 +26,10 @@ namespace TodoList.Api.Tests
         public void Init()
         {
             _controller = new ItemsController
-                {
-                    Configuration = new HttpConfiguration(),
-                    Request = new HttpRequestMessage()
-                };
+            {
+                Configuration = new HttpConfiguration(),
+                Request = new HttpRequestMessage()
+            };
         }
 
 
@@ -42,6 +42,7 @@ namespace TodoList.Api.Tests
             Assert.That(actionResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
             Assert.That(itemsFromMessage, Is.EqualTo(_defaultItems).UsingItemComparer());
         }
+
         [Test]
         public async Task GetAsync_Id_ReturnsFirstItemWithOkStatusCode()
         {
@@ -54,20 +55,21 @@ namespace TodoList.Api.Tests
         }
 
         [Test]
-        public async Task  PostAsync_Item_ReturnsFirstItemWithCreatedStatusCode()
+        public async Task PostAsync_Item_ReturnsFirstItemWithCreatedStatusCode()
         {
-            var mockItem = new Item { Id = new Guid("2daa5641-f500-4021-adc9-47b08806cd6c"), Text = "Item to post" };
+            var mockItem = new Item {Id = new Guid("2daa5641-f500-4021-adc9-47b08806cd6c"), Text = "Item to post"};
             var actionResult = await _controller.ExecuteAction(controller => controller.PostAsync(mockItem));
             actionResult.TryGetContentValue(out Item itemFromMessage);
 
             Assert.That(actionResult.StatusCode, Is.EqualTo(HttpStatusCode.Created));
             Assert.That(itemFromMessage, Is.EqualTo(_defaultItems[0]).UsingItemComparer());
         }
+
         [Test]
         public async Task PutAsync_Id_Item_ReturnSecondItemWithOkStatusCode()
         {
             var itemId = new Guid("f4968efb-4f1e-445b-b907-6ba0ac63bc01");
-            var mockItem = new Item { Id = itemId , Text = "Item to put" };
+            var mockItem = new Item {Id = itemId, Text = "Item to put"};
 
             var actionResult = await _controller.ExecuteAction(controller => controller.PutAsync(itemId, mockItem));
             actionResult.TryGetContentValue(out Item itemFromMessage);
